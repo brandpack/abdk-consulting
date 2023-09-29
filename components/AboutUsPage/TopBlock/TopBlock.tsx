4cfea8f826ae16a65dc9c7c7884029b3d0f2aa4c
isddxy <milyaev.dev@gmail.com>
2023-09-29 15:58:57 +0300

    updated image to about us page

--- components/AboutUsPage/TopBlock/TopBlock.tsx
+++ components/AboutUsPage/TopBlock/TopBlock.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import cls from './TopBlock.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
-import AboutUs from '@/public/AboutUs.webp';
+import AboutUs from '@/public/about-us.webp';
 
 interface TopBlockProps {
 }
