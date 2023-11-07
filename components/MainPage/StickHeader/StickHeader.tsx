7b43b84e6c090c9375be07fe374f16899b2330ad
isddxy <milyaev.dev@gmail.com>
2023-11-07 14:34:40 +0300

    updated responsive style

--- components/MainPage/StickHeader/StickHeader.tsx
+++ components/MainPage/StickHeader/StickHeader.tsx
@@ -11,6 +11,8 @@ const StickHeader: React.FC<any> = (props) => {
     const pathname = usePathname()
     const [isMobile, setIsMobile] = useState(false);
 
+    const isVisible = props.isHeaderFixed;
+
     const scrollToTop = () => {
         if (props.scrollableElementRef.current) {
             props.scrollableElementRef.current.scrollTo({ top: 0, behavior: 'smooth' });
@@ -23,57 +25,43 @@ const StickHeader: React.FC<any> = (props) => {
                 setIsMobile(window.innerWidth <= 700);
             }
         }
-
         handleResize();
-
         window.addEventListener('resize', handleResize);
-
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
-    return (
 
-        <div className={classNames(cls.headerFixed, { [cls.visible]: props.isHeaderFixed }, [])}>
-            <div className={cls.headerContent}>
-                {pathname === '/' ?
-                    <Link className={cls.Logo} href={'/'} onClick={scrollToTop}>
-                        <Image
-                            src={logoDesktop}
-                            alt={'ADKB'}
-                            loading="lazy"
-                        />
-                    </Link>
-                    :
-                    <Link className={cls.Logo} href={'/'}>
-                        <Image
-                            src={logoDesktop}
-                            alt={'ADKB'}
-                            loading="lazy"
-                        />
-                    </Link>
-                }
-                {
-                    isMobile ?
-                        <>
-                            <div
-                                onClick={() => props.setIsOpen(!props.isOpen)}
-                                className={classNames(cls.burger, { [cls.open]: props.isOpen }, [])}
-                            >
-                                <span />
-                                <span />
-                                <span />
-                            </div>
-                        </>
-                        :
-                        <div className={cls.headerLinks}>
-                            <Link href={'/audit'} className={cls.link}>Audit</Link>
-                            <Link href={'/research'} className={cls.link}>Research</Link>
-                            <Link href={'/consulting'} className={cls.link}>Consulting</Link>
-                            <Link href={'/evaluation'} className={cls.link}>Evaluation</Link>
-                            <Link href={'/about-us'} className={cls.link}>About us</Link>
-                        </div>
-                }
+    const BurgerMenu = () => {
+        return(
+            <div onClick={() => props.setIsOpen(!props.isOpen)} className={classNames(cls.burger, { [cls.open]: props.isOpen }, [])}>
+                <span />
+                <span />
+                <span />
+            </div>
+        )
+    }
+
+    const DesktopNavigation = () => {
+        return(
+            <div className={cls.navigation}>
+                <Link href={'/audit'} className={cls.link}>Audit</Link>
+                <Link href={'/research'} className={cls.link}>Research</Link>
+                <Link href={'/consulting'} className={cls.link}>Consulting</Link>
+                <Link href={'/evaluation'} className={cls.link}>Evaluation</Link>
+                <Link href={'/about-us'} className={cls.link}>About us</Link>
+            </div>
+        )
+    }
+
+
+    return (
+        <div className={classNames(cls.header, { [cls.visible]: isVisible }, [])}>
+            <div className={cls.container}>
+                <Link className={cls.logo} href={'/'} onClick={scrollToTop}>
+                    <Image src={logoDesktop} alt={'ABDK - Your guide in the world of crypto | Logotype'} loading="lazy"/>
+                </Link>
+                { isMobile ? BurgerMenu() : DesktopNavigation() }
             </div>
         </div>
     );
