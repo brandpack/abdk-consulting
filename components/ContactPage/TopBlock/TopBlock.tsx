424eb04e8d0d86e14785a4b6c8e6e07187ae6e0b
isddxy <milyaev.dev@gmail.com>
2023-11-08 01:45:11 +0300

    updated responsive style for contact

--- components/ContactPage/TopBlock/TopBlock.tsx
+++ components/ContactPage/TopBlock/TopBlock.tsx
@@ -15,10 +15,12 @@ export const TopBlockContact: FC<TopBlockProps> = ({ }) => {
     return (
         <div className={cls.TopBlock}>
             <h1 className={cls.ApproachHeader}>
-                GET IN TOUCH
+                Get in touch
             </h1>
-            <div className={cls.Container}>
-            <div className={cls.Evaluation4}>
+
+            <div className={cls.container}>
+
+                <div className={cls.left}>
                     <Image
                         src={EmailLink}
                         alt='EmailLink'
@@ -28,59 +30,59 @@ export const TopBlockContact: FC<TopBlockProps> = ({ }) => {
                         <p>Email</p>
                         <div className={cls.FooterBtn}>
                             <a className={cls.EvaluationFooterEmailLink} href="mailto:info@abdk.consulting">info@abdk.consulting</a>
-                            <button
-                                className={cls.EmailCopy}
-                                onClick={() => navigator.clipboard.writeText('info@abdk.consulting')}
-                            >
-                                Copy
-                            </button>
+                            <button className={cls.EmailCopy} onClick={() => navigator.clipboard.writeText('info@abdk.consulting')}>Copy</button>
                         </div>
                         <p className={cls.emailText}>
-                        We combine experience from science and industry and are able to solve problems that are too tough for our competitors.
+                            We combine experience from science and industry and are able to solve problems that are too tough for our competitors.
                         </p>
                     </div>
                 </div>
-            <div className={cls.TopBlockHeader}>
-                <div className={`${cls.Evaluation} ${cls.Evaluation1}`}>
-                    <Image
-                        src={X}
-                        alt='X'
-                        loading='lazy'
-                    />
-                    <Link href={"https://x.com/ABDKconsulting"} className={cls.EvaluationText} target="_blank">
-                        <p>Link</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                            <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
-                        </svg>
-                    </Link>
-                </div>
-                <div className={`${cls.Evaluation} ${cls.Evaluation2}`}>
-                    <Image
-                        src={LinkedIn}
-                        alt='LinkedIn'
-                        loading='lazy'
-                    />
-                    <Link href={"https://www.linkedin.com/company/abdk-consulting/"} className={cls.EvaluationText} target="_blank">
-                        Link
-                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                            <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
-                        </svg>
-                    </Link>
-                </div>
-                <div className={`${cls.Evaluation} ${cls.Evaluation3}`}>
-                    <Image
-                        src={GhLink}
-                        alt='GhLink'
-                        loading='lazy'
-                    />
-                    <Link className={cls.EvaluationText} href={'https://github.com/abdk-consulting'} target="_blank" >
-                        Link
-                        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
-                            <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
-                        </svg>
-                    </Link>
-                </div>
-            </div>    
+
+                <div className={cls.right}>
+
+                    <div className={`${cls.Evaluation} ${cls.Evaluation1}`}>
+                        <Image
+                            src={X}
+                            alt='X'
+                            loading='lazy'
+                        />
+                        <Link href={"https://x.com/ABDKconsulting"} className={cls.EvaluationText} target="_blank">
+                            <p>Link</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+                                <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
+                            </svg>
+                        </Link>
+                    </div>
+                    
+                    <div className={`${cls.Evaluation} ${cls.Evaluation2}`}>
+                        <Image
+                            src={LinkedIn}
+                            alt='LinkedIn'
+                            loading='lazy'
+                        />
+                        <Link href={"https://www.linkedin.com/company/abdk-consulting/"} className={cls.EvaluationText} target="_blank">
+                            Link
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+                                <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
+                            </svg>
+                        </Link>
+                    </div>
+
+                    <div className={`${cls.Evaluation} ${cls.Evaluation3}`}>
+                        <Image
+                            src={GhLink}
+                            alt='GhLink'
+                            loading='lazy'
+                        />
+                        <Link className={cls.EvaluationText} href={'https://github.com/abdk-consulting'} target="_blank" >
+                            Link
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+                                <path d="M0.703125 10.2969L10 1M10 1H-3.72529e-08M10 1V11" strokeWidth="1.6" strokeLinejoin="bevel" />
+                            </svg>
+                        </Link>
+                    </div>
+
+                </div>    
             </div>
         </div>
     )
