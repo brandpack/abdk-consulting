96dbfa0c59329a32ee9a6874f11348f49e778cd7
isddxy <milyaev.dev@gmail.com>
2023-11-07 16:45:44 +0300

    removed old header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,8 +2,6 @@
 import { TopBlockContact } from "@/components/ContactPage/TopBlock/TopBlock";
 import Footer from "@/components/common/Footer/Footer";
 import MobileMenu from "@/components/common/Menu/MobileMenu";
-import StaticHeader from "@/components/common/old_StaticHeader/StaticHeader";
-import StickHeader from "@/components/common/StickHeader/StickHeader";
 import { useState } from "react";
 import TopStaticHeader from "@/components/common/TopStaticHeader/TopStaticHeader";
 
@@ -13,7 +11,6 @@ export default function Audit() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <main className='page'>
-      {/* <StaticHeader isOpen={isOpen} setIsOpen={setIsOpen} /> */}
       <TopStaticHeader isOpen={isOpen} setIsOpen={setIsOpen} auditColor={'white'} researchColor={'white'} consultingColor={'white'} evaluationColor={'white'} />
       <TopBlockContact />
       <Footer />
