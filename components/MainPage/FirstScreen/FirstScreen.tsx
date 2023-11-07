851b92ea13f67b0ab60c078be0149ff4eb04f1e5
isddxy <milyaev.dev@gmail.com>
2023-11-08 00:13:08 +0300

    updated style for bg video

--- components/MainPage/FirstScreen/FirstScreen.tsx
+++ components/MainPage/FirstScreen/FirstScreen.tsx
@@ -2,7 +2,6 @@
 import { FC, useEffect, useState } from 'react'
 import cls from './FirstScreen.module.scss'
 import Link from 'next/link';
-import BgVideo from '../../common/Video/BgVideo';
 
 interface FirstScreenProps {
 }
@@ -13,21 +12,22 @@ export const FirstScreen: FC<FirstScreenProps> = ({ }) => {
     useEffect(() => {
         // Check if window is defined (i.e., we are in a browser environment)
         if (typeof window !== 'undefined') {
-            setIsMobile(window.innerWidth <= 700);
+            setIsMobile(window.innerWidth <= 1000);
         }
     }, []);
 
     const MobileVideo = () => {
         return (
-            <video className={cls.bgVideo} autoPlay={true} muted={true} playsInline={true} disablePictureInPicture={true} loop={true}>
-                <source src='/BgVideoMobile1.mp4' type="video/mp4" />
+            <video autoPlay muted playsInline disablePictureInPicture loop>
+                <source src='/BgVideoMobile1.webm' type="video/webm" />
+                <source src='/BgVideoMobile.mp4' type="video/mp4" />
             </video>
         )
     }
 
     const DestkopVideo = () => {
         return (
-            <video className={cls.bgVideo} autoPlay muted playsInline disablePictureInPicture loop>
+            <video autoPlay muted playsInline disablePictureInPicture loop>
                 <source src='/BgVideo.webm' type="video/webm" />
                 <source src='/BgVideo.mp4' type="video/mp4" />
             </video>
@@ -41,9 +41,7 @@ export const FirstScreen: FC<FirstScreenProps> = ({ }) => {
             <div className={cls.videoWrapper}>
                 { isMobile ? MobileVideo() : DestkopVideo() }
             </div>
-            {/* <div className={cls.LinearGradient}></div> */}
             <div className={cls.container}>
-
                 <h1>Your <b>Guide</b> in the World of Crypto</h1>
                 <div className={cls.bottom}>
                     <Link href={'/contact'} className={cls.button}>
@@ -55,28 +53,5 @@ export const FirstScreen: FC<FirstScreenProps> = ({ }) => {
                 </div>
             </div>
         </div>
-
-        // <div className={cls.TopBlock}>
-        //     <div className={cls.videoWrapper}>
-        //         { isMobile ? MobileVideo() : DestkopVideo() }
-        //     </div>
-        //     {/* <div className={cls.LinearGradient}></div> */}
-        //     <div className={cls.container}>
-        //         <div className={cls.TopBlockHeader}>
-        //             Your <span className={cls.TopBlockGuide}> { logoSymbol() } Guide </span> in the World of Crypto
-        //         </div>
-        //         <div className={cls.TopBlockHeaderFixHeader}>
-        //             Your
-        //         </div>
-        //         <div className={cls.TopBlockFooter}>
-        //             <Link href={'/contact'} className={cls.TopBlockButton}>
-        //                 Contact Us
-        //                 <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-        //                     <path d="M1.57191 15.7969L15.7821 1.58667M15.7821 1.58667H0.497189M15.7821 1.58667V16.8716" stroke="white" strokeWidth="2.29274" strokeLinejoin="bevel" />
-        //                 </svg>
-        //             </Link>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
